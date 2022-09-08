b6c2cc9aff204b59b4221d27416d69d0d8e33060
Eric-Peltzman33 <erpeltz@gmail.com>
2022-09-07 20:38:08 -0400

    updated mongoose

--- server.js
+++ server.js
@@ -1,35 +1,29 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const mongoose = require('mongoose');
-const DB = process.env.DATABASE_URL || 'mongodb://localhost:27017/your-database-name';
-const PORT = process.env.PORT || 3000; 
-const morgan = require('morgan'); // log requests to the console
-const chalk = require('chalk');
-const dotenv = require('dotenv');
-const infoModel = require('./models/infoSchema');
+const chalk = require( 'chalk' );
+const mainRouter = require( './controllers/mainRouter' );
+const mongoose = require( 'mongoose' );
+require( "dotenv" ).config();
+const morgan = require( 'morgan' );
+const PORT = process.env.PORT || 3000;
+
+app.use( express.urlencoded( { extended: true } ) );
+app.use( express.json() );
 
 
-const mainRouter = require('./controllers/mainRouter');
-dotenv.config();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json() );
+app.set( 'view engine', 'ejs' );
+
+app.use( '/', mainRouter ); // use mainRouter for all routes starting with '/'
+
+const url = process.env.MONGO_URL
+mongoose.connect( url, { useNewUrlParser: true, useUnifiedTopology: true } )
+
+	.then( () => app.listen( PORT, () => console.log( chalk.magentaBright( `Server running on port: ${ PORT }` ) ) ) )
+
+	.catch( ( error ) => console.log( chalk.redBright( error.message ) ) )
 
 
-app.set('view engine', 'ejs');
 
-app.use('/', mainRouter); // use mainRouter for all routes starting with /
-
-// connection to database
-mongoose.connect(process.env.Database_URL ).then(() => {
-	console.log( chalk.blue ( 'Connected to database' ) );
-}).catch((err) => {
-	console.log( chalk.red ( `Error connecting to database: ${err}` ) );
-});
-
-// start the server
-app.listen(PORT, () => {
-	console.log( chalk.green ( `Server started on port ${PORT}` ) );
-} );
 
 
 
