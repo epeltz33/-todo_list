27cd26a3a5059b13c0fe61676520f9674ea3e9a3
Eric-Peltzman33 <erpeltz@gmail.com>
2022-09-07 23:57:00 -0400

    commit

--- controllers/mainRouter.js
+++ controllers/mainRouter.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const chalk = require("chalk");
-const mongoose = require("mongoose");
 const Todoinfo = require("../models/infoSchema"); // import the model
-
+ // GET all todos
+ router.get("/:id", async (req, res) => {
+     const todo = await Todoinfo.findOne({ _id: req.params.id });
+     !todo ? res.status(404).send("The todo with the given ID was not found.") : res.send(todo);
+ });
+ //  GET a single todo
 router.get("/", async (req, res) => {
   const allTodos = await Todoinfo.find({}).sort("-date");
 
   res.render("home", { allTodos }); // render the home page and pass the data to it as an object
 });
-
+// POST a new todo
 router.post("/", async (req, res) => {
   const newTodo = new Todoinfo(req.body);
   await newTodo.save((err) => {
@@ -19,13 +22,13 @@ router.post("/", async (req, res) => {
   });
   res.redirect("/");
 });
-
+// DELETE a todo
 router.get("/:id/delete", async (req, res) => {
   const todoDelete = await Todoinfo.findByIdAndDelete(req.params.id);
 
   res.redirect("/");
 });
-
+// UPDATE a todo to completed
 router.get("/:id/finish", async (req, res) => {
   const todoDelete = await Todoinfo.findByIdAndUpdate(req.params.id, {
     progress: "Completed", // update the progress field to finished
@@ -33,13 +36,13 @@ router.get("/:id/finish", async (req, res) => {
 
   res.redirect("/");
 });
-
+// UPDATE a todo to in progress
 router.get("/:id/update", async (req, res) => {
   const updateTodo = await Todoinfo.findById(req.params.id);
 
   res.render("update", { updateTodo });
 });
-
+// UPDATE a todo to completed
 router.get("/:id/update/final", async (req, res) => {
   const updateTodo = await Todoinfo.findByIdAndUpdate(req.params.id, {
     description: req.query.description,
@@ -47,5 +50,6 @@ router.get("/:id/update/final", async (req, res) => {
 
   res.redirect("/");
 });
+ 
 
 module.exports = router;
